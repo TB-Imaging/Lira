7349059856c3bc0b901b55a6b0e5293ed174c92d
jameson <jdri@rams.colostate.edu>
2019-10-01 04:57:12 -0600

    added popups, moved back to kivy

--- src/classify_with_kivy.py
+++ src/classify_with_kivy.py
@@ -1,25 +1,69 @@
 from Dataset import Dataset
 from kivy.app import App
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.relativelayout import RelativeLayout
 from kivy.uix.widget import Widget
+from kivy.uix.checkbox import CheckBox
+from kivy.uix.button import Button
+from kivy.uix.popup import Popup
+from kivy.uix.label import Label
+from kivy.config import Config
 
 Config.set('kivy', 'desktop', True)
 
-class ClassifyWindow(Widget):
+exceptionValues = {
+    "NO_INPUT": 1,
+}
+
+class ClassifyWindow(FloatLayout):
+    pass
+
+
+class CheckBoxBox(RelativeLayout):
+
+    def getChecked(self):
+        for child in self.children:
+            # print(type(CheckBox()))
+            if type(CheckBox()) == type(child):
+                return child.active
+        return False
+
+
+class Bar(RelativeLayout):
+    pass
+
+
+class MenuButton(Button):
     pass
 
 
 class ClassifyApp(App):
+
     def build(self):
-        return ClassifyWindow
+        return ClassifyWindow()
+
+    def beginClassify(self, uid, restart):
+        res, ret = classify(uid, restart)
+        if res == exceptionValues["NO_INPUT"]:
+            popup = Popup(title="No Input",
+                          content=Label(text=str(ret)),
+                          size_hint=(None, None),
+                          size=(300, 300))
+            popup.open()
 
 
-#Main function to put all images through our classification pipeline. Returns the dataset used during the pipeline.
+# Main function to put all images through our classification pipeline. Returns the dataset used during the pipeline.
 def classify(uid, restart):
-    dataset = Dataset(uid=uid, restart=restart)
+    try:
+        dataset = Dataset(uid=uid, restart=restart)
+    except Exception as e:
+        return exceptionValues["NO_INPUT"], e
     dataset.detect_type_ones()
     dataset.predict_grids()
     dataset.get_stats()
     return dataset
+
 
 if __name__ == '__main__':
     ClassifyApp().run()
