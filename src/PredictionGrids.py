c431e92f004dd2117f330e19d6109c80893eab3a
jameson <jdri@rams.colostate.edu>
2019-12-12 11:51:18 -0700

    use cv to optimize prediction grid speed

--- src/PredictionGrids.py
+++ src/PredictionGrids.py
@@ -2,6 +2,7 @@ import sys
 
 import numpy as np
 from keras.models import load_model
+import cv2
 
 from base import *
 from EditingDataset import EditingDataset
@@ -9,6 +10,32 @@ from ImageSubsections import ImageSubsections
 from PredictionGridEditor import PredictionGridEditor
 from post_processing import denoise_predictions
 from ProgressBar import TwoLayerProgress
+
+
+def get_lung_contours(img):
+    height, width, depth = img.shape
+
+    scale = 0.05
+    newWidth, newHeight = int(width * scale), int(height * scale)
+    # print(newWidth, newHeight)
+    small_im = cv2.resize(img, (newWidth, newHeight))
+
+    blurred = cv2.GaussianBlur(small_im, (1, 1), 0)
+    hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+
+    # tissue - red 35 - 255, mean 183 ; green 0 - 232, mean 117; blue 53 - 255, mean 180
+
+    emptyLower = (0, 15, 15)
+    emptyUpper = (255, 255, 255)
+
+    mask = cv2.inRange(hsv, emptyLower, emptyUpper)
+    mask = cv2.dilate(mask, None, iterations=4)
+    mask = cv2.erode(mask, None, iterations=7)
+    mask = cv2.dilate(mask, None, iterations=7)
+
+    im2, contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contourlist = [(c / scale).astype(np.float32) for c in contours if cv2.contourArea(c) > 1000]
+    return contourlist
 
 
 class PredictionGrids(object):
@@ -39,7 +66,6 @@ class PredictionGrids(object):
             # For non-type-one classification
             self.non_type_one_classifier = load_model("../classifiers/non_type_one_classifier.h5")
         else:
-            # print("REPLACE STANDARD KRAMNIK CLASSIFIER WITH BALB/C")
             self.non_type_one_classifier = load_model("../classifiers/balbc_classifier.h5")
 
     def generate(self):
@@ -57,6 +83,9 @@ class PredictionGrids(object):
             progress.setProgressTwoPercent(0)
             progress.update()
 
+            # Get lung contours to speed up classification
+            lung_contours = get_lung_contours(img)
+
             # Total # of predictions on this image
             prediction_h = (img.shape[0] // self.sub_h)
             prediction_w = (img.shape[1] // self.sub_w)
@@ -64,6 +93,9 @@ class PredictionGrids(object):
 
             # Where predictions are stored for image. Starts as 1d for easier reference
             prediction_grid = np.zeros((prediction_n, self.class_n), dtype=np.float32)
+            prediction_grid[:, 3] = 1
+            # prediction_grid = np.full((prediction_n, self.class_n), fill_value=2, dtype=np.float32)
+
 
             # Class to interface with the image as if it were a vector of subsections of size self.sub_hxself.sub_w,
             # without actually dividing the image as doing so would require too much storage.
@@ -98,15 +130,34 @@ class PredictionGrids(object):
             #   which are true. These are the subsections as input for the type-one classifier
             type_one_subsection_indices = np.arange(len(img_subsections))[type_one_mask]
 
+            out_contour_mask = np.ones((prediction_h, prediction_w), dtype=bool)
+            # Test each portion of the grid to see if it lies inside one of the contours
+            for r in range(len(out_contour_mask)):
+                for c in range(len(out_contour_mask[r])):
+                    for i, contour in enumerate(lung_contours):
+                        # Negative if outside
+                        if cv2.pointPolygonTest(contour, (c * self.sub_w, r * self.sub_h), False) > 0 or \
+                                cv2.pointPolygonTest(contour, (c * self.sub_w, r * self.sub_h + self.sub_h),
+                                                     False) > 0 or \
+                                cv2.pointPolygonTest(contour, (c * self.sub_w + self.sub_w,r * self.sub_h, ),
+                                                     False) > 0 or \
+                                cv2.pointPolygonTest(contour,
+                                                     (c * self.sub_w + self.sub_w,r * self.sub_h + self.sub_h, ),
+                                                     False) > 0:
+                            out_contour_mask[r, c] = False
+                            break
+            out_contour_mask = np.reshape(out_contour_mask, (prediction_n,))
             # Get indices of all entries in type_one_mask (and therefore the indices of all subsections which are to
             #   be classified by our non-type-one classifier)
             #   which are false. These are the subsections as input for the non-type-one classifier
-            non_type_one_subsection_indices = np.arange(len(img_subsections))[np.logical_not(type_one_mask)]
+            non_type_one_subsection_indices = np.arange(len(img_subsections))[np.logical_not(
+                np.ma.mask_or(type_one_mask, out_contour_mask))]
 
             # Loop through subsection input indices for both models in batches, get the associated inputs in batches,
             #   then classify the batches (much faster than individual classification) and store back into our prediction grid.
             for type_one_subsection_i in range(0, len(type_one_subsection_indices), self.mb_n):
-                percent = (type_one_subsection_i / len(img_subsections)) * 100.0
+                percent = (type_one_subsection_i / (len(non_type_one_subsection_indices) + len(
+                    type_one_subsection_indices))) * 100.0
                 sys.stdout.write(
                     "\rGenerating Prediction Grid on Image %i/%i. %.2f%% Complete." % (
                         img_i,
@@ -117,6 +168,7 @@ class PredictionGrids(object):
                 progress.setProgressTwoPercent(percent)
                 progress.step()
                 progress.update()
+
                 # Get batch indices
                 type_one_subsection_batch_indices = type_one_subsection_indices[
                                                     type_one_subsection_i:type_one_subsection_i + self.mb_n]
@@ -134,7 +186,7 @@ class PredictionGrids(object):
 
             for non_type_one_subsection_i in range(0, len(non_type_one_subsection_indices), self.mb_n):
                 percent = ((non_type_one_subsection_i + len(type_one_subsection_indices) - 1) /
-                           len(img_subsections)) * 100.0
+                           (len(non_type_one_subsection_indices) + len(type_one_subsection_indices))) * 100.0
                 sys.stdout.write(
                     "\rGenerating Prediction Grid on Image %i/%i. %.2f%% Complete." % (
                         img_i,
@@ -145,10 +197,10 @@ class PredictionGrids(object):
                 progress.setProgressTwoPercent(percent)
                 progress.step()
                 progress.update()
+
                 # Get batch indices
                 non_type_one_subsection_batch_indices = non_type_one_subsection_indices[
                                                         non_type_one_subsection_i:non_type_one_subsection_i + self.mb_n]
-
                 # Get batch inputs from the batch indices
                 non_type_one_subsections = img_subsections[non_type_one_subsection_batch_indices]
 
@@ -156,10 +208,7 @@ class PredictionGrids(object):
                 non_type_one_predictions = self.non_type_one_classifier.predict(non_type_one_subsections)
                 if self.dataset.progress["model"] == "balbc":
                     non_type_one_predictions = np.insert(non_type_one_predictions, 1, 0, axis=1)
-                    # print("ntop:", non_type_one_predictions)
-                    # print("ntop:", non_type_one_predictions[:, 1:])
-                    # print("pg:", prediction_grid[non_type_one_subsection_batch_indices, 2:5].shape)
-                    # print("ntosbi:", non_type_one_subsection_batch_indices)
+
 
                 # Convert the local output classification enumeration of this classifier to the global ones and insert
                 prediction_grid[non_type_one_subsection_batch_indices, 0] = non_type_one_predictions[:, 0]
@@ -174,12 +223,6 @@ class PredictionGrids(object):
             self.after_editing[img_i] = np.argmax(prediction_grid, axis=2)
 
         progress.destroy()
-        # root = ProgressRoot(
-        #     len(self.dataset.imgs),
-        #     "Generating Prediction Grid",
-        #     prediction_callback
-        # )
-        # root.mainloop()
 
         sys.stdout.flush()
         print("")
