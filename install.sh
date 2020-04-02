68496887b02ac71431a5d3bf46af051f8e1ded64
Jameson <masksofpurple@gmail.com>
2020-04-02 14:43:28 -0600

    fix install script to add icon

--- install.sh
+++ install.sh
@@ -4,18 +4,20 @@ python3 -m venv $DIR/venv
 source $DIR/venv/bin/activate
 pip install -r $DIR/requirements.txt
 
-echo "[Desktop Entry]" > ~/.local/share/applications/Lira.desktop
-echo "Version=1.0" >> ~/.local/share/applications/Lira.desktop
-echo "Type=Application" >> ~/.local/share/applications/Lira.desktop
-echo "Terminal=false" >> ~/.local/share/applications/Lira.desktop
-echo "Icon[en_US]=$DIR/lira/icons/xdiagnose-lira.png" >> ~/.local/share/applications/Lira.desktop
-echo "Exec=$DIR/lira/lira.sh" >> ~/.local/share/applications/Lira.desktop
-echo "Name[en_US]=Lira" >> ~/.local/share/applications/Lira.desktop
-echo "Comment[en_US]=Launch L.I.R.A." >> ~/.local/share/applications/Lira.desktop
-echo "Name=Lira" >> ~/.local/share/applications/Lira.desktop
-echo "Comment=Launch L.I.R.A." >> ~/.local/share/applications/Lira.desktop
-echo "Icon=$DIR/lira/icons/xdiagnose-lira.png" >> ~/.local/share/applications/Lira.desktop
-echo "GenericName[en_US]=Classify Bacterial Lesions" >> ~/.local/share/applications/Lira.desktop
+echo "[Desktop Entry]" > Lira.desktop
+echo "Version=1.0" >> Lira.desktop
+echo "Type=Application" >> Lira.desktop
+echo "Terminal=false" >> Lira.desktop
+echo "Icon[en_US]=$DIR/icons/xdiagnose-lira.png" >> Lira.desktop
+echo "Exec=$DIR/lira.sh" >> Lira.desktop
+echo "Name[en_US]=Lira" >> Lira.desktop
+echo "Comment[en_US]=Launch L.I.R.A." >> Lira.desktop
+echo "Name=Lira" >> Lira.desktop
+echo "Comment=Launch L.I.R.A." >> Lira.desktop
+echo "Icon=$DIR/icons/xdiagnose-lira.png" >> Lira.desktop
+echo "GenericName[en_US]=Classify Bacterial Lesions" >> Lira.desktop
+
+sudo mv Lira.desktop /usr/share/applications/Lira.desktop
 
 curl https://downloads.imagej.net/fiji/latest/fiji-linux64.zip --output $DIR/fiji.zip
 unzip $DIR/fiji.zip -d $DIR/src/
